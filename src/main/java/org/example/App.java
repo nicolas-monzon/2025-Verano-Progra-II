6f26a37780237a9b70140e885b38bf72868a825f
Nicolás Monzón <nimonzon@uade.edu.ar>
2025-01-30 14:49:29 -0300

    Added adt Stack

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,15 +1,21 @@
 package org.example;
 
-import org.example.model.ComplejoConParteRealPositiva;
-import org.example.util.ComplejoUtil;
+import org.example.model.Stack;
+import org.example.model.StaticStack;
+
+import static org.example.util.StackUtil.print;
 
 public class App {
 
     public static void main(String[] args) {
-        ComplejoConParteRealPositiva complejo = new ComplejoConParteRealPositiva(3, 0);
-        ComplejoConParteRealPositiva complejo2 = new ComplejoConParteRealPositiva(2, 1);
-
-        ComplejoConParteRealPositiva resultado = ComplejoUtil.suma(complejo, complejo2);
+        Stack stack = new StaticStack();
+        stack.add(1);
+        stack.add(2);
+        stack.add(3);
+        stack.add(4);
+        stack.add(5);
+        print(stack);
+        print(stack);
     }
 
 }
---
+++ src/main/java/org/example/model/StaticStack.java
@@ -0,0 +1,46 @@
+package org.example.model;
+
+public class StaticStack implements Stack {
+
+    private static final int MAX = 10000;
+
+    private final int[] array;
+    private int count;
+
+    public StaticStack() {
+        this.array = new int[MAX];
+        this.count = 0;
+    }
+
+    @Override
+    public int getTop() {
+        if(this.isEmpty()) {
+            throw new RuntimeException("No se puede obtener el tope de una pila vacia");
+        }
+        return this.array[count - 1];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.count == 0;
+    }
+
+    @Override
+    public void add(int a) {
+        this.array[count] = a;
+        this.count++;
+    }
+
+    @Override
+    public void remove() {
+        if(this.isEmpty()) {
+            throw new RuntimeException("No se puede desapilar de una pila vacia");
+        }
+        this.count--;
+    }
+
+    public void f() {
+
+    }
+
+}

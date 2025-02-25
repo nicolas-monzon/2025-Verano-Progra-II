c2edba4e023d7c76455dd8c46b8eee79140b3c50
Nicolás Monzón <nimonzon@uade.edu.ar>
2025-02-25 12:46:27 -0300

    Added dynamic graph

--- src/main/java/org/example/util/QueueUtil.java
+++ src/main/java/org/example/util/QueueUtil.java
@@ -1,11 +1,32 @@
 package org.example.util;
 
+import org.example.model.PriorityQueue;
 import org.example.model.Queue;
+import org.example.model.StaticPriorityQueue;
 import org.example.model.StaticQueue;
 
 public class QueueUtil {
 
     private QueueUtil() {
+
+    }
+
+    public static PriorityQueue phi() {
+        PriorityQueue priorityQueue = new StaticPriorityQueue();
+
+        int[] fibArray = new int[100];
+
+        fibArray[0] = 1;
+        fibArray[1] = 2;
+
+        for(int i = 2; i < 100; i++) {
+            priorityQueue.add(fibArray[i-2], fibArray[i-1]);
+            fibArray[i] = fibArray[i-1] + fibArray[i-2];
+        }
+
+        System.out.println(fibArray[99] / (double) fibArray[98]);
+
+        return priorityQueue;
 
     }
 
